60bc2b1ab8895b7f9c51ace5dab19eb4a6daf22c
Razars <riri22773@gmail.com>
2026-08-04 11:17:58 +0300

    Time: 19 ms (19.38%), Space: 22 MB (79.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dir = {}
         for i in strs:
+            key = "".join(sorted(i))
             if "".join(sorted(i)) not in dir:
-                dir["".join(sorted(i))] = []
-            dir["".join(sorted(i))].append(i)
+                dir[key] = []
+            dir[key].append(i)
         return list(dir.values())
