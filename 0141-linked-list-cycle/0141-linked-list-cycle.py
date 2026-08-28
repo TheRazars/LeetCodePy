6a40566453d8bc8d5299c9af97ba271cfcddc368
Razars <riri22773@gmail.com>
2026-08-28 16:57:29 +0300

    Time: 54 ms (55.53%), Space: 22.6 MB (54.73%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -5,11 +5,13 @@
 #         self.next = None
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return None
         s = head
         f = head
         while f and f.next:
             s = s.next
             f = f.next.next
-            if s == f:
+            if f == s:
                 return True
         return False
